c5b053ac06fa4d7ae2f6485af0563b23df3f1b50
pkyh2 <pkyh1@naver.com>
2022-07-16 03:33:28 +0900

    stoneSwap

---
+++ app.js
@@ -0,0 +1,17 @@
+const { application } = require("express");
+const express = require("express");
+const app = express();
+
+app.set("views", "./views");
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+  res.render("home/index")
+})
+
+app.use(express.static('./assets'))
+app.use(express.static('./style'))
+
+app.listen(3000, () => {
+  console.log("서버 가동")
+})
