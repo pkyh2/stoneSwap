bfdbc057d3a2ebb59ee8246fe362df99b0cdc305
pkyh2 <pkyh1@naver.com>
2022-07-18 14:39:59 +0900

    new_stone_page

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 app.set("views", "./views");
 app.set("view engine", "ejs")
 
+app.get("/", (req, res) => {
+  res.render("home/index")
+})
+
 app.get("/english", (req, res) => {
   res.render("home/english")
 })
@@ -15,7 +19,8 @@ app.get("/chinese", (req, res) => {
 
 app.use(express.static('./assets'))
 app.use(express.static('./style'))
+app.use(express.static('./images'))
 
-app.listen(8080, () => {
+app.listen(3000, () => {
   console.log("서버 가동")
 })
